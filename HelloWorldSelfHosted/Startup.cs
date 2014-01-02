cd27741aff69729e10ac51e8baa4bca89084de96
Alexander Ryndin <ryndin.as@gmail.com>
2014-01-02 20:02:33 +0200

    self hosted hello world project

---
+++ HelloWorldSelfHosted/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+
+namespace HelloWorldSelfHosted
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
