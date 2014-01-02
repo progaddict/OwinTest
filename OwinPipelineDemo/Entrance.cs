ead93330af17b090d86b0572cc0d23ffdb635b8e
Alexander Ryndin <ryndin.as@gmail.com>
2014-01-02 21:27:05 +0200

    pipeline demo.

---
+++ OwinPipelineDemo/Entrance.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Owin;
+using System.IO;
+using System.Web;
+
+[assembly: OwinStartup(typeof(OwinPipelineDemo.Entrance), "Ololo")]
+
+namespace OwinPipelineDemo
+{
+    public class Entrance
+    {
+        public void Ololo(IAppBuilder app)
+        {
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+                return next.Invoke();
+            });
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "2nd MW");
+                return next.Invoke();
+            });
+            app.UseStageMarker(PipelineStage.Authenticate);
+            app.Run(context =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "3rd MW");
+                return context.Response.WriteAsync("Hello world");
+            });
+            app.UseStageMarker(PipelineStage.ResolveCache);
+        }
+        private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string msg)
+        {
+            var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+            context.Get<TextWriter>("host.TraceOutput")
+                .WriteLine("Current IIS event: " + currentIntegratedpipelineStage + " Msg: " + msg);
+        }
+    }
+}
